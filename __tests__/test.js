98faeb21569567ff001f9bcf67c5fce0ad1c2fa9
nage <voronin@vezetvsem.ru>
2018-06-27 00:32:48 +0300

    add test for both formats

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,10 +5,11 @@ const expected = '__tests__/__fixtures__/expected.txt';
 const before = format => `__tests__/__fixtures__/before.${format}`;
 const after = format => `__tests__/__fixtures__/after.${format}`;
 
-const diffTest = format => () => {
-    expect(compareDiff(before(format), after(format)))
-        .toEqual(readFileSync(expected, 'utf8'));
+const diffTest = (formatBefore, formatAfter = formatBefore) => () => {
+    expect(readFileSync(expected, 'utf8'))
+        .toEqual(compareDiff(before(formatBefore), after(formatAfter)));
 };
 
 test('JSON diff', diffTest('json'));
 test('yaml diff', diffTest('yml'));
+test('JSON && yaml diff', diffTest('json', 'yml'));
