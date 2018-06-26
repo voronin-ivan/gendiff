5d9b9491f4dde70dfdbb009820b54af284d27dfd
nage <voronin@vezetvsem.ru>
2018-06-27 00:03:59 +0300

    refactoring for tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,12 +5,10 @@ const expected = '__tests__/__fixtures__/expected.txt';
 const before = format => `__tests__/__fixtures__/before.${format}`;
 const after = format => `__tests__/__fixtures__/after.${format}`;
 
-test('JSON diff', () => {
-    expect(compareDiff(before('json'), after('json')))
+const diffTest = format => () => {
+    expect(compareDiff(before(format), after(format)))
         .toEqual(readFileSync(expected, 'utf8'));
-});
+};
 
-test('yaml diff', () => {
-    expect(compareDiff(before('yml'), after('yml')))
-        .toEqual(readFileSync(expected, 'utf8'));
-});
+test('JSON diff', diffTest('json'));
+test('yaml diff', diffTest('yml'));
