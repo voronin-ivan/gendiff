5c52041a682fe8a94c5e091fa8052cb119ba13d2
nage <voronin@vezetvsem.ru>
2018-06-30 15:12:47 +0300

    refactoring for render

--- src/core/render.js
+++ src/core/render.js
@@ -1,24 +1,26 @@
 import { isPlainObject } from 'lodash';
 
 const renderNode = (node, depth) => {
+  const indent = (count = 0) => `${' '.repeat(depth * 4 + count)}`;
+
   const stringify = (value) => {
     if (!isPlainObject(value)) return value;
 
     const difference = Object.keys(value).reduce((acc, key) =>
-      `${acc}${' '.repeat(depth * 4 + 8)}${key}: ${value[key]}\n`, '\n');
+      `${acc}${indent(8)}${key}: ${value[key]}\n`, '\n');
 
-    return `{${difference}${' '.repeat(depth * 4 + 4)}}`;
+    return `{${difference}${indent(4)}}`;
   };
 
   const renderString = (symbol, key, value) =>
-    `${' '.repeat(depth * 4 + 2)}${symbol} ${key}: ${stringify(value)}\n`;
+    `${indent(2)}${symbol} ${key}: ${stringify(value)}\n`;
 
   const difference = Object.keys(node).reduce((acc, key) => {
     const { type, value } = node[key];
 
     switch (type) {
       case 'deep':
-        return `${acc}${' '.repeat(depth * 4 + 4)}${key}: ${renderNode(value, depth + 1)}\n`;
+        return `${acc}${indent(4)}${key}: ${renderNode(value, depth + 1)}\n`;
       case 'same':
         return `${acc}${renderString(' ', key, value)}`;
       case 'changed':
@@ -34,7 +36,7 @@ const renderNode = (node, depth) => {
     }
   }, '\n');
 
-  return `{${difference}${' '.repeat(depth * 4)}}`;
+  return `{${difference}${indent()}}`;
 };
 
 export default ast => renderNode(ast, 0);
